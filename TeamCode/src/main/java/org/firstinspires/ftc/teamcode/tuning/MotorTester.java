6634af59c370901a6b267b080c220b7108c68c89
karpada2 <gongrossman@gmail.com>
2024-12-23 20:25:40 +0200

    fixed the ftclib importing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/MotorTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/MotorTester.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.tuning;
 
+import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -11,6 +14,13 @@ public class MotorTester extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         DcMotorEx motor = hardwareMap.get(DcMotorEx.class, "motor");
         motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        GamepadEx gamepadEx = new GamepadEx(gamepad1);
+        GamepadButton grabButton = new GamepadButton(
+                gamepadEx, GamepadKeys.Button.A
+        );
+        GamepadButton releaseButton = new GamepadButton(
+                gamepadEx, GamepadKeys.Button.B
+        );
 
         waitForStart();
 
