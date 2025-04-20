f97abb87531d804204fd59f2a688ab25a2354f6f
Electrocaruzo <gongrossman@gmail.com>
2025-04-20 13:16:42 +0300

    created class for controlling an axon servo like a motor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AxonSwerve/EncodedCRServo.java
@@ -0,0 +1,127 @@
+package org.firstinspires.ftc.teamcode.AxonSwerve;
+
+import com.arcrobotics.ftclib.controller.PIDFController;
+import com.qualcomm.robotcore.hardware.AnalogInput;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+
+/*
+    -----------------------------------------------------
+    | This uses degrees cuz they are the easiest for me |
+    -----------------------------------------------------
+ */
+
+
+public class EncodedCRServo {
+    public enum RunMode {
+        RUN_WITH_ENCODER, // Rotates according to the given power and tracks it's position
+        RUN_TO_POSITION // Runs to the given position, in degrees
+    }
+    private CRServo servo;
+    private AnalogInput input;
+    private double position = 0;
+    private double lastAngle;
+    private double targetPosition = 0;
+    private double inputOffset = 0;
+
+    private double power;
+    private static PIDFCoefficients defaultPIDFCoefficients = new PIDFCoefficients(
+            0.001,
+            0,
+            0.001,
+            0.1
+    );
+    private PIDFCoefficients pidfCoefficients;
+    private PIDFController pidfController;
+    private RunMode runMode = RunMode.RUN_TO_POSITION;
+
+    public EncodedCRServo(CRServo servo, AnalogInput input) {
+        this(servo, input, defaultPIDFCoefficients);
+    }
+
+    public EncodedCRServo(CRServo servo, AnalogInput input, PIDFCoefficients pidfCoefficients) {
+        this.servo = servo;
+        this.input = input;
+        resetEncoder();
+        setPidfCoefficients(pidfCoefficients);
+    }
+
+    public void setPidfCoefficients(PIDFCoefficients pidfCoefficients) {
+        setPidfCoefficients(pidfCoefficients.p, pidfCoefficients.i, pidfCoefficients.d, pidfCoefficients.f);
+    }
+
+    public void setPidfCoefficients(double p, double i, double d, double f) {
+        pidfCoefficients = new PIDFCoefficients(p, i, d, f);
+        pidfController.setPIDF(pidfCoefficients.p, pidfCoefficients.i, pidfCoefficients.d, pidfCoefficients.f);
+    }
+
+    private double getCurrentAngle() {
+        return ((input.getVoltage()/input.getMaxVoltage())*360) - inputOffset;
+    }
+
+    private void calculatePosition() {
+        double currAngle = getCurrentAngle();
+        double angleDifference = currAngle - lastAngle;
+
+        if (angleDifference > 180) {
+            angleDifference -= 360;
+        }
+        else if (angleDifference < -180) {
+            angleDifference += 360;
+        }
+        position += angleDifference;
+        lastAngle = currAngle;
+    }
+
+    public double getCurrentPosition() {
+        return position;
+    }
+
+    public double getTargetPosition() {
+        return targetPosition;
+    }
+
+    public void setRunMode(RunMode runMode) {
+        this.runMode = runMode;
+    }
+
+    public RunMode getRunMode() {
+        return runMode;
+    }
+
+    public void resetEncoder() {
+        lastAngle = 0;
+        inputOffset = getCurrentAngle();
+        position = 0;
+        targetPosition = 0;
+    }
+
+    public void setTargetPosition(double targetPosition) {
+        this.targetPosition = targetPosition;
+        pidfController.setSetPoint(targetPosition);
+    }
+
+    public void setPower(double power) {
+        this.power = power;
+    }
+    public double getPower() {
+        return power;
+    }
+
+    public double getPIDFPower() {
+        pidfController.setSetPoint(targetPosition);
+        return pidfController.calculate(position);
+    }
+
+    public void update() {
+        calculatePosition();
+
+        if (runMode == RunMode.RUN_TO_POSITION) {
+            servo.setPower(getPIDFPower());
+        }
+        else {
+            servo.setPower(power);
+        }
+    }
+}
